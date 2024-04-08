06bac3cf07fc701d05b38f89088e6fee5ad40a87
KarloMezdic <165927198+KarloMezdic@users.noreply.github.com>
2024-04-08 23:07:31 +0200

    Update SceneDelegate.swift

--- MovieApp/MovieApp/SceneDelegate.swift
+++ MovieApp/MovieApp/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window=UIWindow(windowScene: windowScene)
-        let vc=LoginViewController()
+        let vc=MovieDetailsViewController()
         window?.rootViewController=vc
         window?.makeKeyAndVisible()
         
